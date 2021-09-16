8e8d3e93477df05ac3d40f931abb21d2ec730fd5
noob-anmol <anmolghai50@gmail.com>
2021-09-17 01:36:22 +0530

    initialised react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import AboutMe from "./components/aboutme";
+import Data from "./components/livingConversation";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/about">
+            <Navbar> </Navbar>
+          </Route>
+          <Route path="/users">
+            <div>Hello World</div>
+          </Route>
+          <Route path="/home">
+              <Navbar></Navbar>
+              <AboutMe></AboutMe>
+          </Route>
+          <Route path="/living-conversation">
+            <Navbar></Navbar>
+            <Data></Data>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
